157503c7c68cbbf216911f86fe60c0d54291ed28
Yoann <yoann.chamillard@gmail.com>
2024-02-07 13:00:39 +0100

    First commit

---
+++ app/src/main/java/com/mydomain/bottomnavigationview/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.mydomain.bottomnavigationview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.nav_view)
+    }
+}
